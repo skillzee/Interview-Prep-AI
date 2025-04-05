361b8bb51e80cad13bd02190faf5202e10929ea5
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-04-05 22:29:40 +0530

    mindway through auth forms

---
+++ components/FormField.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+
+const FormField = () => (
+    <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+)
+
+export default FormField
