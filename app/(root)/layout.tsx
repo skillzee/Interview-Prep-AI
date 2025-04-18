585a0a19cf251691a9aa439b0c5df9ad7a524bc8
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-04-18 17:39:02 +0530

    minor redirect change

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,12 +2,13 @@ import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { isAuthenticated } from '@/lib/actions/auth.action'
+import { redirect } from 'next/navigation'
 
 const RootLayout = async ({children}: {children: ReactNode}) => {
 
   const isUserAuthenticated = await isAuthenticated();
 
-  if(!isUserAuthenticated) return('/sign-in');
+  if(!isUserAuthenticated) redirect('/sign-in');
 
   return (
     <div className='root-layout'>
