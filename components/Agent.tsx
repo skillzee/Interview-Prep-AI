47c748b2f3cdd70856bc6f6f727947358c784965
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-04-22 19:28:23 +0530

    completed the project

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -7,6 +7,7 @@ import React, { useEffect, useState } from 'react'
 
 import {vapi} from '@/lib/vapi.sdk';
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action';
 
 
 
@@ -75,10 +76,11 @@ const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => 
     const handleGenerateFeedback = async(messages: SavedMessage[]) =>{
       console.log('Genrate feedback here.');
 
-      const {success, id} = {
-        success: true,
-        id: 'FeedBack-id'
-      }
+      const {success, feedbackId: id} = await createFeedback({
+        interviewId: interviewId!,
+        userId: userId!,
+        transcript: messages
+      })
 
       if(success && id){
         router.push(`/interview/${interviewId}/feedback`);
